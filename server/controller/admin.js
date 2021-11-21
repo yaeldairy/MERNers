86fde958388f37881341c090d3def39e531176f3
Habiba Gamil <habiba.gamil2000@gmail.com>
2021-11-21 20:44:46 +0200

    controller added

---
+++ server/controller/admin.js
@@ -0,0 +1,20 @@
+const Flight = require ('../db/models/flight');
+const mongoose= require('mongoose');
+
+exports.allFlights = (req, res)= {
+
+    // Flight.find({},(response, error)=>{
+
+    //     if (response){
+    //         res.status(200).send(res)
+    //     }
+    //     else{
+    //         res.status(400).send(err)
+    //     }
+        
+
+    // })
+    
+
+
+}
