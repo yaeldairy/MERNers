e79975045b2ce30a8969c08a708a92beed7fd88b
Nadin Mahmoud <nadin101h@gmail.com>
2021-11-23 19:28:16 +0200

    final end

--- server/controller/admin.js
+++ server/controller/admin.js
@@ -2,17 +2,63 @@ const Flight = require ('../db/models/flight');
 const mongoose= require('mongoose');
 
 exports.allFlights = (req, res)=> {
-
+    
     Flight.find({},(error, response)=>{
+        if (response){
+            res.status(200).send(res)
+        }
+        else{
+            res.status(400).send(err)
+        }
+        
 
+    })
+
+
+}
+
+exports.updateFlight =(req,res)=> {
+
+    const {id,flightNum,deptAirport,arrAirport,deptTime,arrTime,date,nOfEconomy,nOfBusiness} = req.body;
+    console.log(req.body);
+    var objectId = mongoose.Types.ObjectId(id);
+    console.log(objectId);
+
+
+    Flight.findByIdAndUpdate(objectId,{flightNum,deptAirport,arrAirport,deptTime,arrTime,date,nOfEconomy,nOfBusiness},(error, response)=>{
         if (response){
             res.status(200).send(response)
         }
         else{
             res.status(400).send(error)
         }
-        
-
     })
     
+
+
+
 }
+
+
+exports.deleteFlight =(req,res)=> {
+
+    const id  = req.body.id;
+    console.log(req.body);
+    var objectId = mongoose.Types.ObjectId(id);
+    console.log(objectId);
+
+
+    Flight.findByIdAndDelete({objectId},(error, response)=>{
+        if (response){
+            res.status(200).send(response)
+        }
+        else{
+            res.status(400).send(error)
+        }
+    })
+    
+
+
+
+}
+
