93091a0fc6a3a5d9010e10e3f319102ad916b271
Habiba Gamil <habiba.gamil2000@gmail.com>
2021-12-22 17:56:33 +0200

    choose seats hooked

--- server/router/admin.js
+++ server/router/admin.js
@@ -1,25 +1,24 @@
-<<<<<<< HEAD
 const express = require('express')
 const controller= require ('../controller/admin')
 const verifyToken = require ('../Auth/auth.validation.middleware')
 const permissionMiddleware = require ('../Auth/auth.permission.middleware')
+const authorize = require('../auth/middleware');
+const config = require ('../auth/config')
+const jwt = require('jsonwebtoken');
 
 const router = express.Router()
 router.use(express.json())
+
 router.use(verifyToken.validJWTNeeded)
 router.use(permissionMiddleware.AdminPermission)
 
 router.get('/test', controller.testRoute);
 
-=======
-const express = require('express');
-const router = express.Router();
-router.use(express.json());
-const authorize = require('../auth/middleware');
-const config = require ('../auth/config');
 
-const controller= require ('../controller/admin');
-const jwt = require('jsonwebtoken'); //npm install jsonwebtoken
+
+
+
+ //npm install jsonwebtoken
 
 // router.get('/token', authorize(), (req, res)=>{ //do i need authorize?
 //     const payload = {
@@ -31,7 +30,6 @@ const jwt = require('jsonwebtoken'); //npm install jsonwebtoken
 // })
 
 router.get('/flights', authorize("flight:get"), controller.allFlights);
->>>>>>> feature/userProfile
 router.patch('/updateFlight', controller.updateFlight)
 router.post('/deleteFlight', controller.deleteFlight)
 router.post('/flights', [controller.insertFlight]);
